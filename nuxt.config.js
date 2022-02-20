2947fcdf9ea206636fb02482d9367171b720cfb9
0xridwan <100043475+0xridwan@users.noreply.github.com>
2022-02-20 03:29:35 +0100

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   ssr: false,
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Obafunso Ridwan A. - An Outstanding Software Engineer From Nigeria',
+    title: 'Obafunso Ridwan A. - An Outstanding Web Developer From Nigeria',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
